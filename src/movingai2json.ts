84c04d673a8e97d8237ca2eee15a48e5a6e8c96d
Davide Aversa <thek3nger@gmail.com>
2019-04-18 11:08:56 +0200

    Convert main file to TypeScript

--- src/movingai2json.ts
+++ src/movingai2json.ts
@@ -1,13 +1,13 @@
 // Polyfill for startsWith.
 if (!String.prototype.startsWith) {
-    Object.defineProperty(String.prototype, 'startsWith', {
+    Object.defineProperty(String.prototype, "startsWith", {
         enumerable: false,
         configurable: false,
         writable: false,
-        value: function(searchString, position) {
+        value(searchString, position) {
             position = position || 0;
             return this.lastIndexOf(searchString, position) === position;
-        }
+        },
     });
 }
 
@@ -16,18 +16,18 @@ if (!String.prototype.startsWith) {
  * @param map_string The input string.
  * @returns {{}} The JSON object representation of the map.
  */
-function parseMapString (map_string) {
+function parseMapString(map_string: string) {
     // Parse a single command in the map header.
-    var parseCommand = (command_string) => {
-        var splitted = command_string.split(" ");
-        return Object.freeze({name: splitted[0], value: splitted[1]});
+    const parseCommand = (command_string: string) => {
+        const splitted = command_string.split(" ");
+        return Object.freeze({ name: splitted[0], value: splitted[1] });
     };
 
-    var json_map = {};
+    const json_map = {};
 
-    var themap_splitted = map_string.split("\n");
-    var i = 0;
-    var command = themap_splitted[i].trim();
+    const themap_splitted = map_string.split("\n");
+    let i = 0;
+    let command = themap_splitted[i].trim();
     // Parse Header
     while (i < themap_splitted.length && !command.startsWith("map")) {
         if (command.startsWith("height")) {
@@ -43,10 +43,10 @@ function parseMapString (map_string) {
 
     // Parse Map
     json_map.matrix = new Array(json_map.height);
-    for (var r=0;r<json_map.height;r++) {
+    for (let r = 0; r < json_map.height; r++) {
         json_map.matrix[r] = new Array(json_map.width);
-        for (var c=0;c<json_map.width;c++) {
-            json_map.matrix[r][c] = themap_splitted[r+i][c];
+        for (let c = 0; c < json_map.width; c++) {
+            json_map.matrix[r][c] = themap_splitted[r + i][c];
         }
     }
     return json_map;
@@ -58,10 +58,10 @@ function parseMapString (map_string) {
  * @returns {Array} The JSON representation of the input string scenario.
  */
 function parseScenString(scenString) {
-    let scenarios = scenString.split("\n").slice(1); // Split and remove the header.
+    const scenarios = scenString.split("\n").slice(1); // Split and remove the header.
 
-    let parseScenario = (scenarioString) => {
-        const parsed = scenarioString.split('\t');
+    const parseScenario = (scenarioString) => {
+        const parsed = scenarioString.split("\t");
         return {
             bucket: parsed[0],
             map: parsed[1],
@@ -71,13 +71,15 @@ function parseScenString(scenString) {
             startY: parsed[5],
             goalX: parsed[6],
             goalY: parsed[7],
-            optimalLength: parsed[8]
+            optimalLength: parsed[8],
         };
     };
 
-    var scensJSON = [];
-    for (let i=0; i<scenarios.length; i++) {
-        if (scenarios[i].length < 5) { continue; }// Skip empty lines.
+    const scensJSON = [];
+    for (let i = 0; i < scenarios.length; i++) {
+        if (scenarios[i].length < 5) {
+            continue;
+        } // Skip empty lines.
         scensJSON.push(parseScenario(scenarios[i]));
     }
     return scensJSON;
@@ -88,8 +90,8 @@ function parseScenString(scenString) {
  *
  */
 
-var fs = require('fs');
-var async = require('async');
+let fs = require("fs");
+let async = require("async");
 
 /**
  * Get a .map file and produce a file .map.json with the JSON representation of
@@ -98,7 +100,7 @@ var async = require('async');
  * @param onComplete Callback executed when the output file is written.
  */
 function parseMapFile(filePath, onComplete = () => {}) {
-    fs.readFile( filePath, (err, data) => {
+    fs.readFile(filePath, (err, data) => {
         if (err) {
             throw err;
         }
@@ -119,9 +121,9 @@ function parseMapFile(filePath, onComplete = () => {}) {
  * @param onComplete Callback executed when the output file is written.
  */
 function parseScenFile(filePath, onComplete = () => {}) {
-    fs.readFile( filePath, (err, data) => {
+    fs.readFile(filePath, (err, data) => {
         if (err) {
-           throw err;
+            throw err;
         }
         const outJSON = parseScenString(data.toString());
         fs.writeFile(filePath + ".json", JSON.stringify(outJSON), (err) => {
@@ -139,7 +141,7 @@ function parseScenFile(filePath, onComplete = () => {}) {
  * @param onComplete A callback executed when all the files are processed.
  */
 function parseAllInFolder(folder, onComplete = () => {}) {
-    let parseFile = (file, callback) => {
+    const parseFile = (file, callback) => {
         if (file.substr(-4) === ".map") {
             parseMapFile(file, callback);
         } else if (file.substr(-5) === ".scen") {
@@ -147,15 +149,21 @@ function parseAllInFolder(folder, onComplete = () => {}) {
         }
     };
 
-    fs.readdir(folder, (err, files) =>  {
-        files = files.filter( (file) => file.substr(-4) === ".map" || file.substr(-5) === ".scen");
+    fs.readdir(folder, (err, files) => {
+        files = files.filter(
+            (file) => file.substr(-4) === ".map" || file.substr(-5) === ".scen",
+        );
         // console.log(`${files.length} files found!`);
-        async.each(files, (file, callback) => parseFile(folder + "/" + file, callback), (err) => {
-            if (err) {
-                throw err;
-            }
-            onComplete();
-        });
+        async.each(
+            files,
+            (file, callback) => parseFile(folder + "/" + file, callback),
+            (err) => {
+                if (err) {
+                    throw err;
+                }
+                onComplete();
+            },
+        );
     });
 }
 
@@ -171,11 +179,13 @@ exports.parseAllInFolder = parseAllInFolder;
 
 function printUsage(programName) {
     console.log("Usage:");
-    console.log(`${programName} batch [folder] -- Convert all the .map file in the folder path.` );
+    console.log(
+        `${programName} batch [folder] -- Convert all the .map file in the folder path.`,
+    );
 }
 
 if (module.parent === undefined || module.parent === null) {
-    let myArgs = process.argv.slice(2);
+    const myArgs = process.argv.slice(2);
 
     if (myArgs.length < 2) {
         printUsage(process.argv[0] + " " + process.argv[1]);
@@ -186,4 +196,3 @@ if (module.parent === undefined || module.parent === null) {
         }
     }
 }
-
